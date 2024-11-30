01bcf146e6b69864404349187d16efa61b2ee09d
vish <visheshh22@gmail.COM>
2024-11-30 14:25:40 +0530

    netlify config

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/big-basket-clone'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/big-basket-clone/index.html'));});
-app.listen(process.env.PORT || 8080);
